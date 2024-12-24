053b123e5683a1bd2320b419edfa53916eb92ffc
d-lugs <dclustig28@gmail.com>
2024-12-24 12:48:30 -0500

    day 3.2 solution

--- day3/day3.py
+++ day3/day3.py
@@ -1,15 +1,45 @@
+# DAY 3
+
 import re
 
-input = open("input.txt","r")
+def main():
+    input = open("input.txt","r").read()
 
-regex = r"(?<!\w)(mul\(\d{1,3},\d{1,3}\))"
-matches = re.findall(regex, input.read())
+    print(f"part 1: {part1(input)}")
+    print(f"part 2: {part2(input)}")
 
-total = 0
-for string in matches:
-    match = re.search(r"mul\((?P<first>\d{1,3}),(?P<second>\d{1,3})\)",string)
-    first = int(match.group('first'))
-    second = int(match.group('second'))
-    total += first * second
+def part1(input):
+    regex = r"(?<!\w)(mul\(\d{1,3},\d{1,3}\))"
+    matches = re.findall(regex, input)
 
-print(total)
+    total = 0
+    for string in matches:
+        match = re.search(r"mul\((?P<first>\d{1,3}),(?P<second>\d{1,3})\)",string)
+        first = int(match.group('first'))
+        second = int(match.group('second'))
+        total += first * second
+    return total
+
+def part2(input):
+    regex = r"((?<!\w)mul\(\d{1,3},\d{1,3}\)|don't|do)"
+    matches = re.findall(regex, input)
+
+    total = 0
+    condition = True
+    for string in matches:
+        if string == "don't":
+            condition = False
+            continue
+        elif string == "do":
+            condition = True
+            continue
+
+        if condition:
+            match = re.search(r"mul\((?P<first>\d{1,3}),(?P<second>\d{1,3})\)",string)
+            first = int(match.group('first'))
+            second = int(match.group('second'))
+            total += first * second
+    
+    return total
+
+main()
