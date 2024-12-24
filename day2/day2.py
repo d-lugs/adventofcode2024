e87f9318daacbf5b1d39c0eed347648fcb3d6c9e
d-lugs <dclustig28@gmail.com>
2024-12-24 12:07:28 -0500

    day 2.2 solution

--- day2/day2.py
+++ day2/day2.py
@@ -6,71 +6,26 @@ def main():
     input = open("input.txt","r")
     lines = input.read().splitlines()
 
-    count = 0
+    count1 = 0
+    count2 = 0
     for line in lines:
         report = []
         for level in line.split():
             report.append(int(level))
 
-        gradual = is_gradual(report,1)
-        in_order = check_order(report,1)
-        in_order_reverse = check_order(list(reversed(report)),1)
+        gradual = is_gradual(report)
+        in_order = check_order(report)
 
-        if gradual and (in_order or in_order_reverse):
-            count += 1
+        if gradual and in_order:
+            count1 += 1
+            count2 += 1
+        elif with_dampener(report):
+            count2 += 1
 
-    print(count)
+    print(f"without dampener: {count1}")
+    print(f"with dampener: {count2}")
 
-def is_gradual(report, tolerance):
-    gradual = False
-    diffs = []
-    for i in range(len(report)):
-        diff = 0
-        try:
-            diff = report[i+1] - report[i]
-            diffs.append(diff)
-            if abs(diff) < 4:
-                gradual = True
-            else:
-                if tolerance == 0:
-                    gradual = False
-                    break
-                else:
-                    tolerance -= 1
-        except IndexError:
-            pass
-    if 0 in diffs: gradual = False
-    return gradual
-
-def check_order(report, tolerance):
-    # old solution for part 1
-    # in_order = False
-    # if report == sorted(report):
-    #     in_order = True
-    # elif report == sorted(report, reverse=True):
-    #     in_order = True
-    # else:
-    #     in_order = False
-
-    in_order = True
-    for i in range(len(report)):
-        try:
-            if report[i] < report[i+1]:
-                pass
-            else:
-                if tolerance == 0:
-                    in_order = False
-                    break
-                else:
-                    tolerance -= 1
-        except IndexError:
-            pass
-    
-    print(report, in_order)
-
-    return in_order 
-
-def is_safe(report, tolerance):
+def is_gradual(report):
     safe = False
     diffs = []
     for i in range(len(report)):
@@ -81,14 +36,40 @@ def is_safe(report, tolerance):
             if abs(diff) < 4:
                 safe = True
             else:
-                if tolerance == 0:
-                    safe = False
-                    break
-                else:
-                    tolerance -= 1
+                safe = False
+                break
         except IndexError:
             pass
-    if 0 in diffs: safe = False
+    
+    if 0 in diffs: 
+        safe = False
+
+    return safe
+
+def check_order(report):
+    safe = False
+
+    if report == sorted(report):
+        safe = True
+    elif report == sorted(report, reverse=True):
+        safe = True
+    else:
+        safe = False
+
+    return safe
+
+def with_dampener(report):
+    safe = False
+    for i in range(len(report)):
+        temp_report = [i for i in report]
+        del temp_report[i]
+        
+        gradual = is_gradual(temp_report)
+        in_order = check_order(temp_report)
+
+        if gradual and in_order:
+            safe = True
+
     return safe
 
 main()
