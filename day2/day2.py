afae367cb6ad2cf749148682085d2f4a6e28cec8
d-lugs <dclustig28@gmail.com>
2024-12-24 02:14:24 -0500

    working on day2.2

--- day2/day2.py
+++ day2/day2.py
@@ -13,9 +13,10 @@ def main():
             report.append(int(level))
 
         gradual = is_gradual(report,1)
-        in_order = check_order(report)
+        in_order = check_order(report,1)
+        in_order_reverse = check_order(list(reversed(report)),1)
 
-        if gradual and in_order:
+        if gradual and (in_order or in_order_reverse):
             count += 1
 
     print(count)
@@ -41,14 +42,53 @@ def is_gradual(report, tolerance):
     if 0 in diffs: gradual = False
     return gradual
 
-def check_order(report):
-    in_order = False
-    if report == sorted(report):
-        in_order = True
-    elif report == sorted(report, reverse=True):
-        in_order = True
-    else:
-        in_order = False
-    return in_order
+def check_order(report, tolerance):
+    # old solution for part 1
+    # in_order = False
+    # if report == sorted(report):
+    #     in_order = True
+    # elif report == sorted(report, reverse=True):
+    #     in_order = True
+    # else:
+    #     in_order = False
+
+    in_order = True
+    for i in range(len(report)):
+        try:
+            if report[i] < report[i+1]:
+                pass
+            else:
+                if tolerance == 0:
+                    in_order = False
+                    break
+                else:
+                    tolerance -= 1
+        except IndexError:
+            pass
+    
+    print(report, in_order)
+
+    return in_order 
+
+def is_safe(report, tolerance):
+    safe = False
+    diffs = []
+    for i in range(len(report)):
+        diff = 0
+        try:
+            diff = report[i+1] - report[i]
+            diffs.append(diff)
+            if abs(diff) < 4:
+                safe = True
+            else:
+                if tolerance == 0:
+                    safe = False
+                    break
+                else:
+                    tolerance -= 1
+        except IndexError:
+            pass
+    if 0 in diffs: safe = False
+    return safe
 
 main()
