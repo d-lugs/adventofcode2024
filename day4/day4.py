c5b8494f1d562b27a6f6eec5d987c36f7c594943
d-lugs <dclustig28@gmail.com>
2024-12-24 01:22:10 -0500

    day 4 solution

--- day4/day4.py
+++ day4/day4.py
@@ -8,13 +8,20 @@ def main():
     input = open("input.txt","r")
     lines = input.read().splitlines()
 
-    horizontal = findHorizontal(lines)
+    horizontals = findHorizontal(lines)
 
     array = []
     for line in lines:
-        array.append(list(line))
+        if list(line) != []:
+            array.append(list(line))
 
-    print(horizontal)
+    verticals = findVertical(array)
+
+    diagonals = findDiagonal(array)
+
+    total = horizontals + verticals + diagonals
+
+    print(total)
 
 # each function should return a count of all instances of 'xmas' both forwards and backwards
 
@@ -27,9 +34,44 @@ def findHorizontal(lines):
     return count
 
 def findVertical(array):
-    return 0
+    transposed = [[row[i] for row in array] for i in range(len(array[0]))]
+    lines = []
+    for row in transposed:
+        line = ""
+        for char in row:
+            line += char
+        lines.append(line)
+    count = findHorizontal(lines)
+    return count
 
 def findDiagonal(array):
-    return 0
+    col = len(array[0])
+    row = len(array)
+
+    fdiag = [[] for _ in range(col + row - 1)]
+    bdiag = [[] for _ in range(len(fdiag))]
+    min_bdiag = -row + 1
+
+    for x in range(col):
+        for y in range(row):
+            fdiag[x+y].append(array[y][x])
+            bdiag[x-y-min_bdiag].append(array[y][x])
+
+    def getLines(diag):
+        lines = []
+        for row in diag:
+            line = ""
+            for char in row:
+                line += char
+            lines.append(line)
+        return lines
+
+    fdiag_lines = getLines(fdiag)
+    bdiag_lines = getLines(bdiag)
+
+    count = 0
+    count += findHorizontal(fdiag_lines) + findHorizontal(bdiag_lines)
+
+    return count
 
 main()
